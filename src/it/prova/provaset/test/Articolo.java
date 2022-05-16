ab77b73bf63eb3c036bedc66c97f72b7e600477e
ClaudioBuzi001 <buziclaudio@gmail.com>
2022-05-16 14:58:16 +0200

    Commit Finale

---
+++ src/it/prova/provaset/test/Articolo.java
@@ -0,0 +1,9 @@
+package it.prova.provaset.test;
+
+public class Articolo {
+	//TODO CLASSE Articolo che contiene una matricola UNICA
+	//TODO set DI CLASSE CHE CONTIENE TUTTE LE MATRICOLE
+	private String matricola ;
+	//Set<String>	controlloMatricola
+
+}
--- src/it/prova/provaset/test/ProvaSet.java
+++ src/it/prova/provaset/test/ProvaSet.java
@@ -1,4 +1,4 @@
-package provaset;
+package it.prova.provaset.test;
 
 import java.util.HashSet;
 import java.util.Set;
